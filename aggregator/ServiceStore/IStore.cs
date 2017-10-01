9b0d12878cf3a4039b8867ce875f0990c1851be4
Damith <damithasanka@gmail.com>
2017-10-02 01:11:24 +0530

    frist commit

---
+++ aggregator/ServiceStore/IStore.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public interface IStore{
+   void  Add(string key, float value);
+    float Get(string key);
+    bool Exists(string key);
+    IDictionary<string, float> GetAll();
+    void Update(string key, float value);
+}
